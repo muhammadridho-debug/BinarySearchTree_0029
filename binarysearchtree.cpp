da5591c9e65dfa2fbb8ce3d6caef6165d464a17c
muhammadridho-debug <idomuhammad11@gmail.com>
2026-06-11 15:31:46 +0700

    implementasi fungsi inorder traversal

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -67,6 +67,19 @@ public:
                 currentnode = currentnode->rightchild;
         }
     }
+
+    void inorder(node *ptr) 
+    {
+        if (root == NULL) {
+            cout << "tree is empty" << endl;
+            return;
+        }
+        if (ptr != NULL) {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
 };
 
 int main() {
