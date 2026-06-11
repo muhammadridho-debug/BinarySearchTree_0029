9a3ff5edeba9a197202f288ec06300a8737115b1
muhammadridho-debug <idomuhammad11@gmail.com>
2026-06-11 15:19:38 +0700

    Membuat struktur kelas Node

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -2,6 +2,20 @@
 #include <string>
 using namespace std;
 
+class node {
+public:
+    string info;
+    node *leftchild;
+    node *rightchild;
+
+    // constructor for node class
+    node(string i, node *l, node *r) {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
+};
+
 int main() {
     return 0;
 }
